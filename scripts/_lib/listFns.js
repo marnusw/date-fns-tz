de2c3f83c50807a7a25b7392a5d00371effbac88
Marnus Weststrate <marnusw@gmail.com>
2018-11-12 21:03:16 +0000

    Build scripts from date-fns

---
+++ scripts/_lib/listFns.js
@@ -0,0 +1,25 @@
+const path = require('path')
+const fs = require('fs')
+
+module.exports = listFns
+
+const ignoredFiles = [
+  'locale',
+  'esm',
+  'fp',
+  'index.js',
+  'test.js',
+  'index.js.flow',
+  'package.json'
+]
+
+function listFns() {
+  const files = fs.readdirSync(path.join(process.cwd(), 'src'))
+  return files
+    .filter(file => /^[^._]/.test(file) && !ignoredFiles.includes(file))
+    .map(file => ({
+      name: file,
+      path: `./${file}`,
+      fullPath: `./src/${file}/index.js`
+    }))
+}
---
+++ scripts/build/indices.js
@@ -0,0 +1,64 @@
+#!/usr/bin/env node
+
+/**
+ * @file
+ * The script generates index files for submodules.
+ *
+ * It's a part of the build process.
+ */
+
+const fs = require('fs')
+const path = require('path')
+const prettier = require('./_lib/prettier')
+const listFns = require('../_lib/listFns')
+const listFPFns = require('../_lib/listFPFns')
+
+const generatedAutomaticallyMessage =
+  "// This file is generated automatically by `scripts/build/indices.js`. Please, don't change it."
+
+const fns = listFns()
+const fpFns = listFPFns()
+
+writeFile('src/index.js', generateIndex(fns))
+writeFile('src/fp/index.js', generateIndex(fpFns))
+writeFile('src/esm/index.js', generateESMIndex(fns))
+writeFile('src/esm/fp/index.js', generateESMIndex(fpFns))
+
+function writeFile(relativePath, content) {
+  return fs.writeFileSync(
+    path.resolve(process.cwd(), relativePath),
+    prettier(content)
+  )
+}
+
+function generateIndex(files) {
+  const propertyRequireLines = files.map(
+    fn => `  ${fn.name}: require('${fn.path.replace(/\.js$/, '')}/index.js')`
+  )
+
+  const indexLines = [generatedAutomaticallyMessage]
+    .concat('')
+    .concat('module.exports = {')
+    .concat(propertyRequireLines.join(',\n'))
+    .concat('}')
+    .join('\n')
+
+  return `${indexLines}\n`
+}
+
+function generateESMIndex(files) {
+  const fileLines = files.map(
+    fn =>
+      `export { default as ${fn.name} } from '${fn.path.replace(
+        /\.js$/,
+        ''
+      )}/index.js'`
+  )
+
+  const indexLines = [generatedAutomaticallyMessage]
+    .concat('')
+    .concat(fileLines)
+    .join('\n')
+
+  return `${indexLines}\n`
+}
